21a0beb2d4df1642692498a3955cad5f92595121
aditya <aditya@adi-explorer.unl.edu>
2023-08-28 16:02:06 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * Author: Aditya Tadepalli
+ * Date: 2023/08/24
+ *
+ * A simple name and major program in Java
+ * 
+ */
+public class Hello {
+	public static void main(String args[]) {
+	    System.out.println("Name: Aditya Tadepalli");
+	  }
+}
