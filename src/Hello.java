57ca6ff7c3de4bd7a1d18172609581c10f64cffa
colli <colli@LAPTOP-5LJ8DCEC.unl.edu>
2023-08-28 16:30:20 -0500

    Added my name and major

--- src/Hello.java
+++ src/Hello.java
@@ -8,7 +8,10 @@
  */
 public class Hello {
 	public static void main(String args[]) {
-	    System.out.println("Name: Aditya Tadepalli");
-	    System.out.println("Major: Computer Science");
+	    System.out.println("Name: Aditya Tadepalli!");
+	    System.out.println("Major: Computer Science!");
+	    
+	    System.out.println("Name: Collin Siracuse!");
+	    System.out.println("Major: Computer Science!");
 	  }
 }
